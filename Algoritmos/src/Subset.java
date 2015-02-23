a4bf84d12bf0644a732b9c9e6a779102c1351c85
agodi <agodi@hotmail.com>
2015-02-23 15:41:47 -0600

    Algorithms 1 Princeton week 2

---
+++ Algoritmos/src/Subset.java
@@ -0,0 +1,17 @@
+public class Subset {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int j = Integer.valueOf(args[0]);
+		RandomizedQueue<String> queue = new RandomizedQueue<String>();
+
+		while(!StdIn.isEmpty()) {
+			queue.enqueue(StdIn.readString());
+		}
+
+		for (int i=0; i<j; i++) {
+			StdOut.print(queue.dequeue());
+		}
+	}
+
+}
