0fdbc51f99df7c639b1bf81f77039714d43e5dbc
Arturo Gomez <arturo.gomez@autodesk.com>
2015-03-01 22:11:09 -0600

    in place removed

--- Java/DesignAnalysis1/src/com/agodi/MergeSort.java
+++ Java/DesignAnalysis1/src/com/agodi/MergeSort.java
@@ -38,33 +38,4 @@ public class MergeSort {
 		}
 	}
 
-	private static int[] mergeInPlace(int[] array) {
-		if (array.length == 1) {
-			return array;
-		} else {
-			int[] a = merge(Arrays.copyOfRange(array, 0, array.length / 2));
-			int[] b = merge(Arrays.copyOfRange(array, array.length / 2,
-					array.length));
-			int[] c = new int[array.length];
-			int j = 0;
-			int k = 0;
-			for (int i = 0; i < array.length; i++) {
-				if (j >= a.length) {
-					c[i] = b[k];
-					k++;
-				} else if (k >= b.length) {
-					c[i] = a[j];
-					j++;
-				} else if (a[j] <= b[k]) {
-					c[i] = a[j];
-					j++;
-				} else {
-					c[i] = b[k];
-					k++;
-				}
-			}
-			return c;
-		}
-	}
- 
 }
