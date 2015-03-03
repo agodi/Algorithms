1d04792e634502b82e63cf3db9c31da286c8f878
Arturo.Gomez <arturo.gomez@autodesk.com>
2015-03-03 14:34:59 -0600

    corner cases verification added

---
+++ Java/com/agodi/QuickSort.java
@@ -0,0 +1,46 @@
+package com.agodi;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class QuickSort {
+
+	public static void main(String[] args) {
+		int[] array = { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+		sort(array, 0, array.length);
+		System.out.println(Arrays.toString(array));
+	}
+
+	public static void sort(int[] array, int i, int j) {
+        if(i==j) return;
+		int pivotIndex = partition(array, i, j);
+		sort(array, i, pivotIndex);
+		sort(array, pivotIndex + 1, j);
+	}
+
+	private static int partition(int[] array, int i, int j) {
+		System.out.println(Arrays.toString(array));
+		int index = new Random().nextInt(j-i) + i;
+		int pivot = array[index];
+        System.out.println(pivot);
+		array[index] = array[i];
+		index = i;
+		array[i] = pivot;
+        System.out.println(Arrays.toString(array));
+		while(i < j) {
+			while(array[++i] < pivot && i < array.length) {System.out.println(array[i]);}
+			while(array[--j] > pivot && j >=0) {System.out.println(array[j]);}
+            if(i<j) {
+                int aux = array[i];
+                array[i] = array[j];
+                array[j] = aux;
+            }
+			System.out.println(Arrays.toString(array));
+		}
+		array[index] = array[i];
+		array[i] = pivot;
+		System.out.println(Arrays.toString(array));
+		return i;
+	}
+
+}
